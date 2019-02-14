4095862b7c8337bb70468cb202789078df93f7d2
jeff ngo <jeff.ngo@delphix.com>
2019-02-14 15:31:14 -0800

    PYT-179 Virtualization API: Implement discovery (Python wrappers)
    Reviewed at: http://reviews.delphix.com/r/46410/

--- platform/src/main/python/dlpx/virtualization/platform/wrappers.py
+++ platform/src/main/python/dlpx/virtualization/platform/wrappers.py
@@ -26,17 +26,18 @@ Todo:
    http://google.github.io/styleguide/pyguide.html
 
 """
-from dlpx.virtualization import platform_pb2
+from dlpx.virtualization import platform_pb2, common_pb2
 
 
 def configure_wrapper(configure_request):
     """Configure operation wrapper.
 
   Executed just after cloning the captured data and mounting it to a target
-  environment. Specifically, this hook is run during provision and refresh,
-  prior to taking the initial snapshot of the clone. This toolkit hook is run
-  before the user-customizable Configure Clone and Before Refresh hooks are
-  run. It must return a sourceConfig object that represents the new dataset.
+  environment. Specifically, this plugin operation is run during provision and
+  refresh, prior to taking the initial snapshot of the clone. This plugin
+  operation is run before the user-customizable Configure Clone and Before
+  Refresh operations are run. It must return a sourceConfig object that
+  represents the new dataset.
 
   Configure the data to be usable on the target environment. For database data
   files, this may mean recovering from a crash consistent format or backup.
@@ -55,5 +56,71 @@ def configure_wrapper(configure_request):
         repository=configure_request.repository,
         snapshot=configure_request.snapshot)
     configure_response = platform_pb2.ConfigureResponse()
-    configure_response.return_value.source_config.json = config.to_json()
+    configure_response.return_value.source_config.parameters.json = config.to_json()
     return configure_response
+
+
+def repository_discovery_wrapper(repository_discovery_request):
+    """Repository discovery wrapper.
+
+  Executed just after adding or refreshing an environment. This plugin
+  operation is run prior to discovering source configs. This plugin operation
+  returns a list of repositories installed on a environment.
+
+  Discover the repositories on an environment given a source connection.
+
+  Args:
+      repository_discovery_request (RepositoryDiscoveryRequest): Repository
+      Discovery operation arguments.
+
+  Returns:
+      RepositoryDiscoveryResponse: The return value of repository discovery
+      operation.
+  """
+    def to_protobuf(repository):
+        parameters = common_pb2.PluginDefinedObject()
+        parameters.json = repository.to_json()
+        repository_protobuf = common_pb2.Repository()
+        repository_protobuf.parameters.CopyFrom(parameters)
+        return repository_protobuf
+
+    repositories = repository_discovery(
+        source_connection=repository_discovery_request.source_connection)
+    repository_discovery_response = platform_pb2.RepositoryDiscoveryResponse()
+    repository_protobuf_list = map(to_protobuf, repositories)
+    repository_discovery_response.return_value.repositories.extend(repository_protobuf_list)
+    return repository_discovery_response
+
+
+def source_config_discovery_wrapper(source_config_discovery_request):
+    """Source config discovery wrapper.
+
+  Executed when adding or refreshing an environment. This plugin operation is
+  run after discovering repositories and before persisting/updating repository
+  and source config data in MDS. This plugin operation returns a list of source
+  configs from a discovered repository.
+
+  Discover the source configs on an environment given a discovered repository.
+
+  Args:
+      source_config_discovery_request (SourceConfigDiscoveryRequest): Source
+      Config Discovery arguments.
+
+  Returns:
+      SourceConfigDiscoveryResponse: The return value of source config
+      discovery operation.
+  """
+    def to_protobuf(source_config):
+        parameters = common_pb2.PluginDefinedObject()
+        parameters.json = source_config.to_json()
+        source_config_protobuf = common_pb2.SourceConfig()
+        source_config_protobuf.parameters.CopyFrom(parameters)
+        return source_config_protobuf
+
+    source_configs = source_config_discovery(
+        source_connection=source_config_discovery_request.source_connection,
+        repository=source_config_discovery_request.repository)
+    source_config_discovery_response = platform_pb2.SourceConfigDiscoveryResponse()
+    source_config_protobuf_list = map(to_protobuf, source_configs)
+    source_config_discovery_response.return_value.source_configs.extend(source_config_protobuf_list)
+    return source_config_discovery_response
