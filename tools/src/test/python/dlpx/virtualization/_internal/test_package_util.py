49fb696f5f9e4fe35bd23b401574c1f957f6ff1d
Ravi Mukkamala <ravi.mukkamala@delphix.com>
2020-01-29 14:51:37 -0800

    PYT-1081 Merge SDK master to plugin-upgrade branch (follow-on, update settings.cfg)

--- tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
+++ tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
@@ -9,7 +9,7 @@ from dlpx.virtualization._internal import package_util
 class TestPackageUtil:
     @staticmethod
     def test_get_version():
-        assert package_util.get_version() == '1.1.0-internal-upgrade-008'
+        assert package_util.get_version() == '1.1.0-internal-upgrade-009'
 
     @staticmethod
     def test_get_virtualization_api_version():
