80fdf426423c1f42695ec28ad4116258a8bfb62d
Tom Walsh <tom.walsh@delphix.com>
2020-07-22 23:29:04 -0400

    Add support for extended hooks

--- tools/src/main/python/dlpx/virtualization/_internal/commands/build.py
+++ tools/src/main/python/dlpx/virtualization/_internal/commands/build.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019 by Delphix. All rights reserved.
+# Copyright (c) 2019, 2020 by Delphix. All rights reserved.
 #
 
 import base64
@@ -216,6 +216,12 @@ def prepare_upload_artifact(plugin_config_content, src_dir, schemas, manifest):
 
     if plugin_config_content.get('luaName'):
         artifact['luaName'] = plugin_config_content['luaName']
+
+    if plugin_config_content.get('extendedStartStopHooks'):
+        artifact['extendedStartStopHooks'] = plugin_config_content[
+            'extendedStartStopHooks']
+    else:
+        artifact['extendedStartStopHooks'] = False
 
     if plugin_config_content.get('minimumLuaVersion'):
         artifact['minimumLuaVersion'] = plugin_config_content[
--- tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
+++ tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
@@ -10,7 +10,7 @@ import pytest
 class TestPackageUtil:
     @staticmethod
     def test_get_version():
-        assert package_util.get_version() == '2.1.0.dev3'
+        assert package_util.get_version() == '2.1.0.dev4'
 
     @staticmethod
     def test_get_virtualization_api_version():
