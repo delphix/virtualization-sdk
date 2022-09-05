a9eb011d69cd94067b6613b784efc56601fbcfe7
Sourabh <sourabh.jain@delphix.com>
2022-09-05 21:07:37 +0530

    Bump Version 4.0.5 -> 4.0.6.dev0

--- tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
+++ tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
@@ -10,7 +10,7 @@ import pytest
 class TestPackageUtil:
     @staticmethod
     def test_get_version():
-        assert package_util.get_version() == '4.0.5'
+        assert package_util.get_version() == '4.0.6.dev0'
 
     @staticmethod
     def test_get_virtualization_api_version():
