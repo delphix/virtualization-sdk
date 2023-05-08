cf2962aad91a74b16f9ec89872a6f950e293767b
Sourabh <sourabh.jain@delphix.com>
2023-05-08 14:49:38 +0530

    CP-10003 Update dvp-api version to 1.8.0.dev0 in libs, platform and common modules

--- platform/setup.py
+++ platform/setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(PYTHON_SRC, 'dlpx/virtualization/platform/VERSION')) as v
     version = version_file.read().strip()
 
 install_requires = [
-    "dvp-api == 1.7.1.dev0",
+    "dvp-api == 1.8.0.dev0",
     "dvp-common == {}".format(version),
     "six >= 1.16, < 1.17",
 ]
--- tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
+++ tools/src/test/python/dlpx/virtualization/_internal/test_package_util.py
@@ -8,7 +8,7 @@ import pytest
 from dlpx.virtualization._internal import package_util
 
 DVP_VERSION = '4.1.0'
-DVP_API_VERSION = '1.7.1'
+DVP_API_VERSION = '1.8.0'
 
 
 class TestPackageUtil:
