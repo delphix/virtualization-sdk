5e5ab542c60c3dd5e5717a7cc7f23f10c957edab
Sourabh Jain <45626610+SumoSourabh@users.noreply.github.com>
2024-11-12 12:35:45 +0530

    CP-11885 Update the dvp-api version to 1.9.0 in virtualization code (#539)

--- libs/setup.py
+++ libs/setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(PYTHON_SRC, 'dlpx/virtualization/libs/VERSION')) as versi
     version = version_file.read().strip()
 
 install_requires = [
-    "dvp-api == 2.0.0.dev0",
+    "dvp-api == 1.9.0.dev0",
     "dvp-common == {}".format(version),
     "six >= 1.16, < 1.17",
 ]
