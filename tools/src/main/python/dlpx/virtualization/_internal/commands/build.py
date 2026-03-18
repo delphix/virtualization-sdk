555cf56559f736e30a859510efb68ec92b852e32
Rakshanda Bhat <rakshanda.bhat@delphix.com>
2026-03-18 11:15:01 +0530

    Applied patch from Sourabh

--- tools/src/main/python/dlpx/virtualization/_internal/commands/build.py
+++ tools/src/main/python/dlpx/virtualization/_internal/commands/build.py
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 TYPE = 'Plugin'
 LOCALE_DEFAULT = 'en-us'
 VIRTUAL_SOURCE_TYPE = 'PluginVirtualSourceDefinition'
+VIRTUAL_TO_PHYSICAL_TYPE = 'PluginVirtualToPhysicalDefinition'
 DISCOVERY_DEFINITION_TYPE = 'PluginDiscoveryDefinition'
 STAGED_LINKED_SOURCE_TYPE = 'PluginLinkedStagedSourceDefinition'
 DIRECT_LINKED_SOURCE_TYPE = 'PluginLinkedDirectSourceDefinition'
@@ -245,7 +246,7 @@ def prepare_upload_artifact(plugin_config_content, src_dir, schemas, manifest):
             'parameters': schemas['virtualSourceDefinition']
         },
         'virtualToPhysicalDefinition': {
-            'type': VIRTUAL_TO_PHYSICAL_DEFINITION_TYPE,
+            'type': VIRTUAL_TO_PHYSICAL_TYPE,
             'parameters': schemas['virtualToPhysicalDefinition']
         },
         'linkedSourceDefinition': {
